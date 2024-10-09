b1b9eb47ac4eee824e88215e529f7e753fd8a024
Santiago-School <a.23308051140421@cbtis128.edu.mx>
2024-10-08 20:49:55 -0600

    Add files via upload

---
+++ Practica2.2/practica2.2.py
@@ -0,0 +1,10 @@
+print(" ")
+print("Francisco Santiago Carrasco Correa 0421 3°W 08/10/24")
+print(" ")
+# funcion que despliega un saludo para un nombre dado
+def saludo(nombre):
+    return f"hola {nombre}"
+
+# Uso
+print(saludo("santiagoo"))
+print(" ")
