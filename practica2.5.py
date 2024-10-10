f85a59b1a138dea702d8f5663b002d68d7b1280a
Santiago-School <a.23308051140421@cbtis128.edu.mx>
2024-10-09 19:59:36 -0600

    Add files via upload

---
+++ practica2.5.py
@@ -0,0 +1,19 @@
+print(" ")
+print("Francisco Santiago Carrasco Correa 0421 3°W 09/10/24")
+print(" ")
+
+#define el radio y la altura
+radio = 6
+altura = 12
+
+#se realiza la variable constante y los calculos
+pi = 3.141592653589793
+areacirculo = pi * radio ** 2
+volumencilindro = areacirculo * altura
+
+#se utilizan los print para poner los valores obtenidos
+print("area del circulo:", areacirculo)
+print("volumen del cilindro:", volumencilindro)
+
+#pdd: Segun yo el volumen de un circulo no es un termino correcto, 
+#ya que un circulo es una figura bidimensional y no tiene volumen y por ende no lo adjunte
