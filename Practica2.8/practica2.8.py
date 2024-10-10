e0a7caf68ebb1d77f4ee44b9baa07b100a207b2d
Santiago-School <a.23308051140421@cbtis128.edu.mx>
2024-10-09 20:09:28 -0600

    Add files via upload

---
+++ Practica2.8/practica2.8.py
@@ -0,0 +1,15 @@
+print("Francisco Santiago Carrasco Correa 0421 3°W 09/10/24")
+print(" ")
+
+def mayordetres(num1, num2, num3):
+    # comparamos los números y encontramos el mayor
+    return max(num1, num2, num3)
+
+# ejemplo de uso
+numero1 = 6
+numero2 = 26
+numero3 = 16
+
+mayor = mayordetres(numero1, numero2, numero3)
+
+print("el mayor de los tres numeros es:", mayor)
